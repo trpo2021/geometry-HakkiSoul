c6c25aabf636234fbcd3a54870f0347bc9ef1c3a
Никита Чеботоревский <HakkiSoul@users.noreply.github.com>
2021-03-08 19:35:04 +0700

    Create function data_check and improve some output messages

--- geometry.c
+++ geometry.c
@@ -4,18 +4,61 @@
 #include <ctype.h>
 #define K 50
 
+int data_check(int s, char* input_array)
+{
+    int j = 0;
+    if(input_array[s] == ' ') {
+        j++;
+    } else {
+        while (input_array[s] != ' ') {
+            if ((input_array[s] != '-') && (!(((input_array[s] <= '9') && (input_array[s] >= '0')) || (input_array[s] == '.'))) && (input_array[s] != ' ')) {
+                j++;
+                break;
+            } else {
+                s++;
+            }
+        }
+
+        while ((input_array[s] != ',') && (input_array[s + 1] != ' ')) {
+            if ((input_array[s] != '-') && (!(((input_array[s] <= '9') && (input_array[s] >= '0')) || (input_array[s] == '.'))) && (input_array[s] != ' ')) {
+                j++;
+                break;
+            } else {
+                s++;
+            }
+        }
+
+        if (input_array[s + 1] != ' ') {
+            s++;
+        }
+
+        s++;
+
+        while (input_array[s] != ')') { 
+            if ((input_array[s] != '-') && (!(((input_array[s] <= '9') && (input_array[s] >= '0')) || (input_array[s] == '.'))) && (input_array[s] != ' ')) {
+                j++;
+                break;
+            } else {
+                s++;
+            }
+        }
+    }
+
+    return j;
+}
+
 int main()
 {
-    int radius = 0;
-    int i, j, k; //счётчики
     char figure[6] = "Circle";
     char array[K];
     char input_array[K] = {0};
-    float area = 0, perimetr = 0;
 
     FILE *psyh = NULL;
 
     psyh = fopen("text.txt", "r");
+    
+    int i;
+
     if (!psyh) {
         printf("Cant open this file\n");
     } else {
@@ -28,7 +71,7 @@ int main()
 
     fclose(psyh);
 
-    k = 0;
+    int k = 0;
     for(i = 0; i < 6; i++) {
         if(input_array[i] != figure[i]) {
             k = k + 1;
@@ -37,73 +80,52 @@ int main()
     }
 
     int s = 7;
-    j = 0;
-    while (input_array[s] != ' ') {
-        if ((input_array[s] != '-') && (!(((input_array[s] <= '9') && (input_array[s] >= '0')) || (input_array[s] == '.'))) && (input_array[s] != ' ')) {
-            j++;
-            break;
-        } else {
-            s++;
-        }
-    }
-
-
-    while ((input_array[s] != ',') && (input_array[s + 1] != ' ')) {
-        if ((input_array[s] != '-') && (!(((input_array[s] <= '9') && (input_array[s] >= '0')) || (input_array[s] == '.'))) && (input_array[s] != ' ')) {
-            j++;
-            break;
-        }
-        s++;
-    }
-
-    if (input_array[s + 1] != ' ') {
-        s++;
-    }
-
-    s++;
-
-    while (input_array[s] != ')') {
-        if ((input_array[s] != '-') && (!(((input_array[s] <= '9') && (input_array[s] >= '0')) || (input_array[s] == '.'))) && (input_array[s] != ' ')) {
-            j++;
-            break;
-        } else {
-            s++;
-        }
-    }
+    int j = 0;
+    int m;
+    
+    m = data_check(s, input_array);
 
     if (k > 0) {
         for(j = 0; j < K; j++) {
             printf("%c", input_array[j]);
         }
-        printf("incorrect figure name\n");
+
+        printf("\n");
+        printf("Invalid figure name\n");
+
     } else {
-        if (j > 0) {
-            for(j = 0; j < K; j++) {
-                printf("%c", input_array[j]);
+        if (m > 0) {
+            for(m = 0; m < K; m++) {
+                printf("%c", input_array[m]);
             }
-            printf("incorrect value\n");
+
+            printf("\n");
+            printf("Invalid value\n");
+
         } else {
-            for (j = 0; j < K; j++) {
-                if ((input_array[j] == ',') && (input_array[j + 1] == ' ')) {
-                    for (k = j; input_array[k + 1] != ')'; k++) {
-                        array[k - j] = input_array[k + 1];
+            for (m = 0; m < K; m++) {
+                if ((input_array[m] == ',') && (input_array[m + 1] == ' ')) {
+                    for (k = m; input_array[k + 1] != ')'; k++) {
+                        array[k - m] = input_array[k + 1];
                     }
                 }
 
             }
 
+            float area = 0, perimetr = 0;
+            int radius = 0;
+
             radius = atoi(array);
             area = M_PI * radius * radius;
             perimetr = 2 * M_PI * radius;
 
-            printf("\n");
-            for(j = 0; j < K; j++) {
-                printf("%c", input_array[j]);
+            for(m = 0; m < K; m++) {
+                printf("%c", input_array[m]);
             }
 
             printf("\n");
-            printf("perimeter = %.3f\n",perimetr);
-            printf("area = %.3f\n",area);
+            printf("perimeter = %.3f\n", perimetr);
+            printf("area = %.3f\n", area);
         }
 
     }
