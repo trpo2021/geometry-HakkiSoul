d0b235871815d4acf36e9fa2c8d041ed4a0f9fda
Никита Чеботоревский <alek.morozow2018@yandex.ru>
2021-04-13 18:25:41 +0700

    Add some tests for my data check's

--- test/test.c
+++ test/test.c
@@ -5,3 +5,123 @@ CTEST(suite, system_testing)
 {
     ASSERT_EQUAL(1 + 2, 3);
 }
+
+CTEST(figur_name_check, incorrect_figure_1)
+{
+    char str[] = {"c"};
+
+    int real = figur_name_check(&str);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(figur_name_check, incorrect_figure_2)
+{
+    char str[] = {"8circle"};
+
+    int real = figur_name_check(&str);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(figur_name_check, incorrect_figure_2)
+{
+    char str[] = {"Circle"};
+
+    int real = figur_name_check(&str);
+
+    int exp = 0;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(figur_name_check, incorrect_figure_2)
+{
+    char str[] = {" Circle"};
+
+    int real = figur_name_check(&str);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(figur_name_check, incorrect_figure_2)
+{
+    char str[] = {"circle"};
+
+    int real = figur_name_check(&str);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(data_check, incorrect_figure_2)
+{
+    char str[] = {"(0 0, 8)"};
+
+    int s = 1;
+
+    int real = data_check(s, &str);
+
+    int exp = 0;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(data_check, incorrect_figure_2)
+{
+    char str[] = {"( 0 0, 8)"};
+
+    int s = 1;
+
+    int real = data_check(s, &str);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(data_check, incorrect_figure_2)
+{
+    char str[] = {"(0, 8)"};
+
+    int s = 1;
+
+    int real = data_check(s, &str);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(data_check, incorrect_figure_2)
+{
+    char str[] = {"(0 0 8)"};
+
+    int s = 1;
+
+    int real = data_check(s, &str);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(data_check, incorrect_figure_2)
+{
+    char str[] = {"(0 0, 8"};
+
+    int s = 1;
+
+    int real = data_check(s, &str);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
