9efefc745a7af1387b26b80e187e2efa77bb925c
Никита Чеботоревский <alek.morozow2018@yandex.ru>
2021-04-13 19:20:25 +0700

    Improve my tests

--- test/test.c
+++ test/test.c
@@ -6,120 +6,120 @@ CTEST(suite, system_testing)
     ASSERT_EQUAL(1 + 2, 3);
 }
 
-CTEST(figur_name_check, incorrect_figure_1)
+CTEST(figur_name_check, incorrect_name)
 {
-    char str[] = {"c"};
+    char str[2] = {"c"};
 
-    int real = figur_name_check(&str);
+    int real = figur_name_check(str);
 
     int exp = 1;
 
     ASSERT_EQUAL(exp, real);
 }
 
-CTEST(figur_name_check, incorrect_figure_2)
+CTEST(figur_name_check, incorrect_name)
 {
-    char str[] = {"8circle"};
+    char str[8] = {"8circle"};
 
-    int real = figur_name_check(&str);
+    int real = figur_name_check(str);
 
     int exp = 1;
 
     ASSERT_EQUAL(exp, real);
 }
 
-CTEST(figur_name_check, incorrect_figure_2)
+CTEST(figur_name_check, incorrect_name)
 {
-    char str[] = {"Circle"};
+    char str[7] = {"Circle"};
 
-    int real = figur_name_check(&str);
+    int real = figur_name_check(str);
 
     int exp = 0;
 
     ASSERT_EQUAL(exp, real);
 }
 
-CTEST(figur_name_check, incorrect_figure_2)
+CTEST(figur_name_check, incorrect_name)
 {
-    char str[] = {" Circle"};
+    char str[8] = {" Circle"};
 
-    int real = figur_name_check(&str);
+    int real = figur_name_check(str);
 
     int exp = 1;
 
     ASSERT_EQUAL(exp, real);
 }
 
-CTEST(figur_name_check, incorrect_figure_2)
+CTEST(figur_name_check, incorrect_name)
 {
-    char str[] = {"circle"};
+    char str[7] = {"circle"};
 
-    int real = figur_name_check(&str);
+    int real = figur_name_check(str);
 
     int exp = 1;
 
     ASSERT_EQUAL(exp, real);
 }
 
-CTEST(data_check, incorrect_figure_2)
+CTEST(data_check, incorrect_data)
 {
-    char str[] = {"(0 0, 8)"};
+    char str[9] = {"(0 0, 8)"};
 
     int s = 1;
 
-    int real = data_check(s, &str);
+    int real = data_check(s, str);
 
     int exp = 0;
 
     ASSERT_EQUAL(exp, real);
 }
 
-CTEST(data_check, incorrect_figure_2)
+CTEST(data_check, incorrect_data)
 {
-    char str[] = {"( 0 0, 8)"};
+    char str[10] = {"( 0 0, 8)"};
 
     int s = 1;
 
-    int real = data_check(s, &str);
+    int real = data_check(s, str);
 
     int exp = 1;
 
     ASSERT_EQUAL(exp, real);
 }
 
-CTEST(data_check, incorrect_figure_2)
+CTEST(data_check, incorrect_data)
 {
-    char str[] = {"(0, 8)"};
+    char str[7] = {"(0, 8)"};
 
     int s = 1;
 
-    int real = data_check(s, &str);
+    int real = data_check(s, str);
 
     int exp = 1;
 
     ASSERT_EQUAL(exp, real);
 }
 
-CTEST(data_check, incorrect_figure_2)
+CTEST(data_check, incorrect_data)
 {
-    char str[] = {"(0 0 8)"};
+    char str[8] = {"(0 0 8)"};
 
     int s = 1;
 
-    int real = data_check(s, &str);
+    int real = data_check(s, str);
 
     int exp = 1;
 
     ASSERT_EQUAL(exp, real);
 }
 
-CTEST(data_check, incorrect_figure_2)
+CTEST(data_check, incorrect_data)
 {
-    char str[] = {"(0 0, 8"};
+    char str[8] = {"(0 0, 8"};
 
     int s = 1;
 
-    int real = data_check(s, &str);
+    int real = data_check(s, str);
 
     int exp = 1;
 
