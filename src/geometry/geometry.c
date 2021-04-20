163ca0966ad0717013319a593ee82e97e4e76156
Никита Чеботоревский <alek.morozow2018@yandex.ru>
2021-04-20 17:38:43 +0700

    Format geometry.c

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -11,13 +11,11 @@ int main()
 
     fuf = fopen("text.txt", "r");
 
-    if (!fuf) 
-    {
+    if (!fuf) {
         printf("Can`t open the file\n");
     } else {
-        while(!feof(fuf))
-        {
-            char x_array[K] = {0};            
+        while (!feof(fuf)) {
+            char x_array[K] = {0};
             char y_array[K] = {0};
             char r_array[K] = {0};
 
@@ -25,44 +23,37 @@ int main()
 
             fgets(input_array, 40, fuf);
             printf("\n");
-            
+
             int s = 7;
             int check_1 = 0, check_2 = 0;
 
-            if ((check_1 = figur_name_check(input_array)) > 0) 
-            {
-                for (int i = 0; i < K; i++) 
-                {
+            if ((check_1 = figur_name_check(input_array)) > 0) {
+                for (int i = 0; i < K; i++) {
                     printf("%c", input_array[i]);
                 }
                 printf("Invalid name of figure\n");
             } else {
-                if ((check_2 = data_check(s, input_array)) > 0) 
-                {
-                    for (int i = 0; i < K; i++) 
-                    {
+                if ((check_2 = data_check(s, input_array)) > 0) {
+                    for (int i = 0; i < K; i++) {
                         printf("%c", input_array[i]);
                     }
                     printf("Invalid value\n");
                 } else {
-                    for (int i = 0; i < K; i++) 
-                    {
-
-                        if (input_array[i] == '(') 
-                        {                            
-                            for (int k = i; input_array[k + 1] != ' '; k++) 
-                            {
+                    for (int i = 0; i < K; i++) {
+                        if (input_array[i] == '(') {
+                            for (int k = i; input_array[k + 1] != ' '; k++) {
                                 x_array[k - i] = input_array[k + 1];
                             }
                         }
 
-                        if (input_array[i] == ' ') {                              
-                            for (int k = i - 1; input_array[k] != ','; k++) { 
+                        if (input_array[i] == ' ') {
+                            for (int k = i - 1; input_array[k] != ','; k++) {
                                 y_array[k - i] = input_array[k];
                             }
-                        } 
+                        }
 
-                        if ((input_array[i] == ',') && (input_array[i + 1] == ' ')) {
+                        if ((input_array[i] == ',')
+                            && (input_array[i + 1] == ' ')) {
                             for (int k = i; input_array[k + 1] != ')'; k++) {
                                 r_array[k - i] = input_array[k + 1];
                             }
@@ -81,7 +72,7 @@ int main()
                     a = area(radius);
                     p = perimetr(radius);
 
-                    printf("Circle №%d: ", circles_number); 
+                    printf("Circle №%d: ", circles_number);
 
                     for (int i = 0; i < K; i++) {
                         printf("%c", input_array[i]);
@@ -98,10 +89,9 @@ int main()
     fclose(fuf);
     printf("\n");
 
-    if(circles_number > 1)
-    {
-        circle_intersects (circles_number, circles_data);
+    if (circles_number > 1) {
+        circle_intersects(circles_number, circles_data);
     }
-    
+
     return 0;
 }
